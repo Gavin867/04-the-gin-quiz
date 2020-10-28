f6f0b3368336988c5f017408e12f0f25b9b70e0b
Gavin Calkins <gavin.calkins@gmail.com>
2020-10-27 23:05:12 -0700

    psuedo code added

---
+++ Develop/JS/script.js
@@ -0,0 +1,13 @@
+//Create a start quiz button
+
+//Create a timer function that is activated when the start quiz button is clicked
+
+    //Create a function to take wrong answers and subtract 10 seconds
+
+//Create text modifier for the questions
+
+//Create text modifier for the answer options
+
+//Create buttons for questions
+
+//Create a function to count correct answers
