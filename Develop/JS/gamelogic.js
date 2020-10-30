40ac60fe357a87245f175105e9bc2817516dbab7
Gavin Calkins <gavin.calkins@gmail.com>
2020-10-30 16:13:33 -0700

    post tutoring session updates to js, score page and storage mostly

--- Develop/JS/gamelogic.js
+++ Develop/JS/gamelogic.js
@@ -11,3 +11,40 @@
 //Create buttons for questions
 
 //Create a function to count correct answers
+var practiceScore = 75
+
+
+
+var submitScoreBtn=document.getElementById("sbmt-int-btn");
+
+function savePlayerScoreInitials(event) {
+    event.preventDefault ();
+
+    console.log("button-click");
+
+    var playerInitials=document.getElementById("initials-form");
+    
+    console.log(playerInitials.value);
+
+    var currentScore = {
+        score: practiceScore,
+        initals: playerInitials.value
+    }
+
+    console.log(currentScore)
+
+    //getlocalStorageItem, new variable to hold value
+    var highscores =
+      JSON.parse(window.localStorage.getItem("hi-scr-lst")) || [];
+
+      //adding the new player score
+      highscores.push(currentScore);
+
+      //combiming old and new players scores and adding them to storage
+      localstorage.setItem("hi-scr-lst", JSON.stringify(highscores));
+}
+
+submitScoreBtn.onclick=savePlayerScoreInitials;
+
+
+
