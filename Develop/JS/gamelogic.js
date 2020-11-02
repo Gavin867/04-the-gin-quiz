d1c14945284c88aef04c9193950868685a810d48
Gavin Calkins <gavin.calkins@gmail.com>
2020-11-01 22:52:17 -0800

    creating question array

--- Develop/JS/gamelogic.js
+++ Develop/JS/gamelogic.js
@@ -1,5 +1,8 @@
 //The timer
-var t
+
+var time = 0;
+
+
 
 //Create a start quiz button
 
@@ -12,9 +15,59 @@ var t
 
 //Create text modifier for the answer options
 
+var questions = [{
+    question: "Name that cocktail: 1.5 oz London Dry Gin, 1.5 oz Dry Vermouth, 2 drops West Indian Orange Bitters, Stir with ice, Strain into cocktail glass, express lemon peel over beverage, garnish with olive or lemon twist.",
+    answers: ["Tom Collins", "Gimlet", "Martini", "Aviation"],
+    correctAnswer: 2
+}, {
+    question: "Name that cocktail: 2 oz London Dry Gin, 1 oz Lemon Juice, 3/4 oz Simple Syrup, build in a highball glass with ice, stir, top off with club soda and garnish with lemon slice",
+    answers: ["", "", "", ""],
+    correctAnswer: 2
+}, {
+    question: "Name that cocktail: ",
+    answers: ["", "", "", ""],
+    correctAnswer: 1
+}, {
+    question: "Name that cocktail:",
+    answers: ["", "", "", ""],
+    correctAnswer: 0
+}, {
+    question: "Name that cocktail: ",
+    answers: ["", "", "", ""],
+    correctAnswer: 0
+},{
+	question: "Name that cocktail: ",
+    answers: ["", "", "", ""],
+    correctAnswer: 1
+},{
+	question: "Name that cocktail: ",
+    answers: ["", "", "", ""],
+    correctAnswer: 3
+},{
+	question: "Name that cocktail: ",
+    answers: ["", "", "", ""],
+    correctAnswer: 2
+},{
+	question: "Name that cocktail: ",
+    answers: ["", "", "", ""],
+    correctAnswer: 1
+},{
+	question: "Name that cocktail:",
+    answers: ["", "", "", ""],
+    correctAnswer: 0
+}];
+
 //Create buttons for questions
 
 //Create a function to count correct answers
+
+
+
+
+
+
+
+
 var practiceScore = 75
 
 
@@ -39,13 +92,13 @@ function savePlayerScoreInitials(event) {
 
     //getlocalStorageItem, new variable to hold value
     var highscores =
-      JSON.parse(window.localStorage.getItem("hi-scr-lst")) || [];
+      JSON.parse(window.localStorage.getItem("high-score-list")) || [];
 
       //adding the new player score
       highscores.push(currentScore);
 
       //combiming old and new players scores and adding them to storage
-      localstorage.setItem("hi-scr-lst", JSON.stringify(highscores));
+      localstorage.setItem("high-score-list", JSON.stringify(highscores));
 }
 
 submitScoreBtn.onclick=savePlayerScoreInitials;
