ac36b0eab9ad0d17aa9a52265be86ab8dff69543
Gavin Calkins <gavin.calkins@gmail.com>
2020-11-02 16:34:56 -0800

    js question string answer solution

--- Develop/JS/gamelogic.js
+++ Develop/JS/gamelogic.js
@@ -19,77 +19,77 @@ var timerInterval;
 // Define Questions (Array of Objects)
 var questionsList = [{
     question: "1.5 oz London Dry Gin, 1.5 oz Dry Vermouth, 2 drops orange bitters, stir with ice, strain into cocktail glass, express lemon peel over beverage, garnish with olive or lemon twist",
-    answers: ["Tom Collins", "Gimlet", "Martini", "Aviation"],
+    answers: ["a. Tom Collins", "b. Gimlet", "c. Martini", "d. Aviation"],
     correctAnswer: 2
     // Correct answer: Martini
 }, {
     question: "2 oz London dry gin, 1 oz Lemon Juice, 3/4 oz simple syrup, build in a highball glass with ice, stir, top off with club soda and garnish with lemon wheel",
-    answers: ["Box Car", "Corpse Reviver No.2", "Gin Fizz", "Tom Collins"],
+    answers: ["a. Box Car", "b. Corpse Reviver No.2", "c. Gin Fizz", "d. Tom Collins"],
     correctAnswer: 3
     // Correct answer: Tom Collins
 }, {
     question: "2 oz gin, 1/2 tsp Creme de Violette, 1/2 oz maraschino liqueur, 3/4 oz lemon juice, 1/4 oz simple syrup, shake with ice and strain into a coupe glass, garnish with brandied cherry",
-    answers: ["French 75", "Aviation", "Devel's Own", "Overhead Smash"],
+    answers: ["a. French 75", "b. Aviation", "c. Devel's Own", "d. Overhead Smash"],
     correctAnswer: 1
     // Correct answer: Aviation
 }, {
     question: "1.5 oz London dry gin, 1 oz sweet vermouth, 1 oz Campari, stir with ice and strain into a double rocks glass, garnish with orange twist",
-    answers: ["Negroni", "Gonzalez", "Bee's Knees", "Gin Rickey"],
+    answers: ["a. Negroni", "b. Gonzalez", "c. Bee's Knees", "d. Gin Rickey"],
     correctAnswer: 0
     // Correct answer: Negroni
 }, {
     question: "2 oz gin, 1.5 oz lime cordial, shake with ice and strain into a cocktail glass, garnish wtih lime wedge",
-    answers: ["Gimlet", "Zelli's Special", "Mortal Enemy", "Bitter French"],
+    answers: ["a. Gimlet", "b. Zelli's Special", "c. Mortal Enemy", "d. Bitter French"],
     correctAnswer: 0
     // Correct answer: Gimlet
 }, {
 	question: "'Three measures of Gordon's, one of vodka, half a measure of Kina Lillet. Shake it very well unitl it's ice-cold, then add a large, thin slice of lemon peel.'",
-    answers: ["Celine Fizz", "Strange Brew", "Martini", "Vesper"],
+    answers: ["a. Celine Fizz", "b. Strange Brew", "c. Martini", "d. Vesper"],
     correctAnswer: 3
     // Correct answer: Vesper
 }, {
 	question: "1.5 oz London dry gin, 1/2 oz orange liqueur, 3/4 oz lime juice, 1 tsp grenadine, shake with ice and strain into a cocktail glass",
-    answers: ["Box Car", "Heart-Shaped Box", "Hyde Park Swizzle", "Le Bateleur"],
+    answers: ["a. Box Car", "b. Heart-Shaped Box", "c. Hyde Park Swizzle", "d. Le Bateleur"],
     correctAnswer: 0
     // Correct answer: Box Car
 }, {
 	question: "1.5 oz London dry gin, 3/4 oz lemon juice, 2 tsp simple syrup, shake with ice and strain into a flute, top with chilled champagne",
-    answers: ["Ace Cocktail", "French 75", "The Commandant", "Monk's Respite"],
+    answers: ["a. Ace Cocktail", "b. French 75", "c. The Commandant", "d. Monk's Respite"],
     correctAnswer: 1
     // Correct answer: French 75
 }, {
 	question: "2 oz London dry gin, 3/4 oz lemon juice, 3/4 oz simple syrup, egg white, shake with ice and strain into highball glass, top with club soda",
-    answers: ["Petticoat", "Tom Collins", "Gin Fizz", "Alembic"],
+    answers: ["a. Petticoat", "b. Tom Collins", "c. Gin Fizz", "d. Alembic"],
     correctAnswer: 2
     // Correct answer: Gin Fizz
 }, {
 	question: "3/4 oz London dry gin, 2 dashes absinthe, 3/4 oz Lillet Blanc, 3/4 oz cointreau, 3/4 oz lemon juice, shake with ice, strain into coupe glass",
-    answers: ["Summer Shack", "The Bloodbath", "Corpse Reviver No.1", "Corpse Reviver No.2"],
+    answers: ["a. Summer Shack", "b. The Bloodbath", "c. Corpse Reviver No.1", "d. Corpse Reviver No.2"],
     correctAnswer: 3
     // Correct answer: Corpse Reviver No.2
 }, {
 	question: "2 oz London dry gin, 1oz lime juice, 1/2 oz simple syrup, shake with ice, strain into highball glass filled with ice, top with club soda, garnish with lime wedge",
-    answers: ["Bramble", "Mortal Enemy", "Gin Rickey", "Tailspin"],
+    answers: ["a. Bramble", "b. Mortal Enemy", "c. Gin Rickey", "d. Tailspin"],
     correctAnswer: 2
     // Correct answer: Gin Rickey
 }, {
 	question: "2 oz London dry gin, 1 tsp to 1 oz water (to taste), 1 tsp granulated sugar, combine in rocks glass, stir until sugar dissolved, add one large ice cube",
-    answers: ["Gin Sling (standard)", "Singapore Sling", "Black Market Sling", "Pillow Talk"],
+    answers: ["a. Gin Sling (standard)", "b. Singapore Sling", "c. Black Market Sling", "d. Pillow Talk"],
     correctAnswer: 0
     // Corect answer: Gin Sling
 }, {
 	question: "2 oz London dry gin, 1 tsp Creme de Mure, 1 tsp maraschino liqueur, 1/2 oz lime juice, 3/4 oz grapefruit juice, 1 tsp simple syrup, shake with ice and strain into a coupe glass",
-    answers: ["Pink Flag", "Pink Elephant", "Pink Lady", "Sherry Berry Bramble"],
+    answers: ["a. Pink Flag", "b. Pink Elephant", "c. Pink Lady", "d. Sherry Berry Bramble"],
     correctAnswer: 1 
     // Correct answer: Pink Elephant
 }, {
 	question: "2 oz London dry gin, 3/4 oz lemon juice, 3/4 oz honey syrup, 3 drops lavender bitters, shake with ice and strain into a coupe glass, garnish with brandied cherry",
-    answers: ["Bottany of Desire", "Tea's Knees", "Bee's Knees", "Fog Cutter"],
+    answers: ["a. Bottany of Desire", "b. Tea's Knees", "c. Bee's Knees", "d. Fog Cutter"],
     correctAnswer: 2
     // Correct answer: Bee's Knees
 }, {
 	question: "1 oz London dry gin, 1/2 oz cognac, 1/2 oz sweet vermouth, 1/2 oz dry vermouth, 2 dashes aromatic bitters, stir with ice and strain into a cocktail glass, express lemon peel over beverage",
-    answers: ["Frisco Club", "Queen Palm", "Delmonico No.2", "Delmonico No.1"],
+    answers: ["a. Frisco Club", "b. Queen Palm", "c. Delmonico No.2", "d. Delmonico No.1"],
     correctAnswer: 3
     // Correct answer: Delmonico No.1
 }];
