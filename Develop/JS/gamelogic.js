3d08c03b6910e0936bd2fd4b69e580ac27502701
Gavin Calkins <gavin.calkins@gmail.com>
2020-11-09 14:40:34 -0800

    fixed the end of the array issue and post quiz timer display

--- Develop/JS/gamelogic.js
+++ Develop/JS/gamelogic.js
@@ -16,7 +16,7 @@ var timeLeftelement = document.getElementById("timeLeft");
 var playerScoreEl = document.getElementById("playerScore");
 var revealInputPage = document.getElementById("storeScore");
 var submitScoreBtn = document.getElementById("submitInitials");
-var timerInterval; 
+var timerInterval;
 
 
 // Define Questions (Array of Objects)
@@ -41,50 +41,50 @@ var questionsList = [{
     answers: ["a. Gimlet", "b. Zelli's Special", "c. Mortal Enemy", "d. Bitter French"],
     correctAnswer: "a. Gimlet"
 }, {
-	question: "RECIPE: Three measures of Gordon's, one of vodka, half a measure of Kina Lillet. Shake it very well unitl it's ice-cold, then add a large, thin slice of lemon peel.",
+    question: "RECIPE: Three measures of Gordon's, one of vodka, half a measure of Kina Lillet. Shake it very well unitl it's ice-cold, then add a large, thin slice of lemon peel.",
     answers: ["a. Celine Fizz", "b. Strange Brew", "c. Martini", "d. Vesper"],
     correctAnswer: "d. Vesper"
 }, {
-	question: "RECIPE: 1.5 oz London dry gin, 1/2 oz orange liqueur, 3/4 oz lime juice, 1 tsp grenadine, shake with ice and strain into a cocktail glass.",
+    question: "RECIPE: 1.5 oz London dry gin, 1/2 oz orange liqueur, 3/4 oz lime juice, 1 tsp grenadine, shake with ice and strain into a cocktail glass.",
     answers: ["a. Box Car", "b. Heart-Shaped Box", "c. Hyde Park Swizzle", "d. Le Bateleur"],
     correctAnswer: "a. Box Car"
 }, {
-	question: "RECIPE: 1.5 oz London dry gin, 3/4 oz lemon juice, 2 tsp simple syrup, shake with ice and strain into a flute, top with chilled champagne.",
+    question: "RECIPE: 1.5 oz London dry gin, 3/4 oz lemon juice, 2 tsp simple syrup, shake with ice and strain into a flute, top with chilled champagne.",
     answers: ["a. Ace Cocktail", "b. French 75", "c. The Commandant", "d. Monk's Respite"],
     correctAnswer: "b. French 75"
 }, {
-	question: "RECIPE: 2 oz London dry gin, 3/4 oz lemon juice, 3/4 oz simple syrup, egg white, shake with ice and strain into highball glass, top with club soda.",
+    question: "RECIPE: 2 oz London dry gin, 3/4 oz lemon juice, 3/4 oz simple syrup, egg white, shake with ice and strain into highball glass, top with club soda.",
     answers: ["a. Petticoat", "b. Tom Collins", "c. Gin Fizz", "d. Alembic"],
     correctAnswer: "c. Gin Fizz"
 }, {
-	question: "RECIPE: 3/4 oz London dry gin, 2 dashes absinthe, 3/4 oz Lillet Blanc, 3/4 oz cointreau, 3/4 oz lemon juice, shake with ice, strain into coupe glass.",
+    question: "RECIPE: 3/4 oz London dry gin, 2 dashes absinthe, 3/4 oz Lillet Blanc, 3/4 oz cointreau, 3/4 oz lemon juice, shake with ice, strain into coupe glass.",
     answers: ["a. Summer Shack", "b. The Bloodbath", "c. Corpse Reviver No.1", "d. Corpse Reviver No.2"],
     correctAnswer: "d. Corpse Reviver No.2"
 }, {
-	question: "RECIPE: 2 oz London dry gin, 1oz lime juice, 1/2 oz simple syrup, shake with ice, strain into highball glass filled with ice, top with club soda, garnish with lime wedge.",
+    question: "RECIPE: 2 oz London dry gin, 1oz lime juice, 1/2 oz simple syrup, shake with ice, strain into highball glass filled with ice, top with club soda, garnish with lime wedge.",
     answers: ["a. Bramble", "b. Mortal Enemy", "c. Gin Rickey", "d. Tailspin"],
     correctAnswer: "c. Gin Rickey"
 }, {
-	question: "RECIPE: 2 oz London dry gin, 1 tsp to 1 oz water (to taste), 1 tsp granulated sugar, combine in rocks glass, stir until sugar dissolved, add one large ice cube.",
+    question: "RECIPE: 2 oz London dry gin, 1 tsp to 1 oz water (to taste), 1 tsp granulated sugar, combine in rocks glass, stir until sugar dissolved, add one large ice cube.",
     answers: ["a. Gin Sling (standard)", "b. Singapore Sling", "c. Black Market Sling", "d. Pillow Talk"],
     correctAnswer: "a. Gin Sling (standard)"
 }, {
-	question: "RECIPE: 2 oz London dry gin, 1 tsp Creme de Mure, 1 tsp maraschino liqueur, 1/2 oz lime juice, 3/4 oz grapefruit juice, 1 tsp simple syrup, shake with ice and strain into a coupe glass.",
+    question: "RECIPE: 2 oz London dry gin, 1 tsp Creme de Mure, 1 tsp maraschino liqueur, 1/2 oz lime juice, 3/4 oz grapefruit juice, 1 tsp simple syrup, shake with ice and strain into a coupe glass.",
     answers: ["a. Pink Flag", "b. Pink Elephant", "c. Pink Lady", "d. Sherry Berry Bramble"],
-    correctAnswer: "b. Pink Elephant" 
+    correctAnswer: "b. Pink Elephant"
 }, {
-	question: "RECIPE: 2 oz London dry gin, 3/4 oz lemon juice, 3/4 oz honey syrup, 3 drops lavender bitters, shake with ice and strain into a coupe glass, garnish with brandied cherry.",
+    question: "RECIPE: 2 oz London dry gin, 3/4 oz lemon juice, 3/4 oz honey syrup, 3 drops lavender bitters, shake with ice and strain into a coupe glass, garnish with brandied cherry.",
     answers: ["a. Bottany of Desire", "b. Tea's Knees", "c. Bee's Knees", "d. Fog Cutter"],
     correctAnswer: "c. Bee's Knees"
 }, {
-	question: "RECIPE: 1 oz London dry gin, 1/2 oz cognac, 1/2 oz sweet vermouth, 1/2 oz dry vermouth, 2 dashes aromatic bitters, stir with ice and strain into a cocktail glass, express lemon peel over beverage.",
+    question: "RECIPE: 1 oz London dry gin, 1/2 oz cognac, 1/2 oz sweet vermouth, 1/2 oz dry vermouth, 2 dashes aromatic bitters, stir with ice and strain into a cocktail glass, express lemon peel over beverage.",
     answers: ["a. Frisco Club", "b. Queen Palm", "c. Delmonico No.2", "d. Delmonico No.1"],
     correctAnswer: "d. Delmonico No.1"
 }];
 
 // When the user starts the quiz the following will happen
 function startTheQuiz(event) {
-    
+
     // Console registers the button click
     console.log("button-click");
 
@@ -96,12 +96,12 @@ function startTheQuiz(event) {
 
     // Sets the css styling to the questions
     revealQuiz.setAttribute("class", "question-container ");
-    
+
     // Display questions
     displayQuestions();
-    
+
     // Begin the countdown on the timer
-    timerInterval=setInterval(beginCountDown, 1000)
+    timerInterval = setInterval(beginCountDown, 1000)
 }
 
 // The countdown timer functionality
@@ -110,11 +110,8 @@ function beginCountDown() {
     // Countdown by the interval indentified in timerInterval
     time--
 
-    // Display time left as a text value in the timer element of the html
-    timeLeftelement.textContent=time
-
     // When the time runs out (i.e. reaches zero) do the following...
-    if (time<=0) {
+    if (time <= 0) {
 
         playerScoreEl.textContent = score + "/" + questionsList.length;
 
@@ -123,13 +120,20 @@ function beginCountDown() {
 
         // Apply the css styling to the store score page
         storeScore.setAttribute("class", "store-container");
-        
+
         // Clear the timer
         clearInterval(timerInterval);
-        
+
+        // Set timer to zero
+        time = 0;
+
         // Reapply 'hide' css class to make the quiz section invisible
-        revealQuiz.setAttribute("class", "hide")
+        revealQuiz.setAttribute("class", "hide");
     }
+
+    // Display time left as a text value in the timer element of the html
+    timeLeftelement.textContent = time
+
 }
 
 // Tie the function which starts the quiz with the click event of the startButton
@@ -171,7 +175,7 @@ function checkAnswer() {
     if (this.textContent !== questionsList[currentQuestionIndex].correctAnswer) {
         // Show that the answer is incorrect in the console
         console.log("Incorrect Answer")
-    
+
         // removeAtrribute("class") to reveal "incorrect" message
         displayIncorrect.removeAttribute("class");
 
@@ -179,12 +183,12 @@ function checkAnswer() {
         displayIncorrect.setAttribute("class", "response");
 
         // Message becomes hidden again after 1 second
-        setTimeout(()=> {
+        setTimeout(() => {
             displayIncorrect.setAttribute("class", "hide")
         }, 500);
 
         // Subtract 10 second time penalty from time remaining
-        time=time-10;
+        time = time - 10;
 
         // Show message indicating the time penalty
         console.log("-10 Second Time Penalty!")
@@ -193,10 +197,10 @@ function checkAnswer() {
         console.log(score);
     }
 
-    else if (this.textContent === questionsList[currentQuestionIndex].correctAnswer) {   
+    else if (this.textContent === questionsList[currentQuestionIndex].correctAnswer) {
         // Show that the answer is correct in the console
         console.log("Correct Answer")
-        
+
         // removeAtrribute("class") to reveal "correct" message
         displayCorrect.removeAttribute("class");
 
@@ -204,10 +208,10 @@ function checkAnswer() {
         displayCorrect.setAttribute("class", "response");
 
         // Message becomes hidden again after 1 second
-        setTimeout(()=> {
+        setTimeout(() => {
             displayCorrect.setAttribute("class", "hide")
         }, 500);
-        
+
         // Add a point to the score
         score++;
 
@@ -217,7 +221,37 @@ function checkAnswer() {
 
     // Move to next question
     currentQuestionIndex++;
-    displayQuestions();
+
+    // When the last question int the index is reached then render the submit initials page
+    if (currentQuestionIndex === questionsList.length) {
+
+        playerScoreEl.textContent = score + "/" + questionsList.length;
+
+        // Remove the 'hide' css class to display the store score page
+        storeScore.removeAttribute("class");
+
+        // Apply the css styling to the store score page
+        storeScore.setAttribute("class", "store-container");
+
+        // Clear the timer
+        clearInterval(timerInterval);
+
+        // Set timer to zero
+        time = 0;
+
+        // Reapply 'hide' css class to make the quiz section invisible
+        revealQuiz.setAttribute("class", "hide");
+
+        // Display time left as a text value in the timer element of the html
+        timeLeftelement.textContent = time
+    }
+
+    // If its not the last question, keep displaying the questions
+    else {
+
+        // Calls the displayQuestions function
+        displayQuestions();
+    }
 }
 
 //Create a function to store intials and score
@@ -233,7 +267,7 @@ function savePlayerScoreInitials(event) {
     console.log("button-click");
 
     // Create a variable to use the input initials
-    var playerInitials=document.getElementById("initialsForm");
+    var playerInitials = document.getElementById("initialsForm");
 
     // Log the form value for dislpay
     console.log(playerInitials.value);
@@ -249,18 +283,18 @@ function savePlayerScoreInitials(event) {
     // getlocalStorageItem, new variable to hold value
     var highscores =
 
-      // Retrieve the data from the DOM
-      JSON.parse(window.localStorage.getItem("high-score-list")) || [];
-      
-      //adding the new player score
-      highscores.push(currentScore);
-      
-      //combiming old and new players scores and adding them to storage
-      window.localStorage.setItem("high-score-list", JSON.stringify(highscores));
+        // Retrieve the data from the DOM
+        JSON.parse(window.localStorage.getItem("high-score-list")) || [];
+
+    //adding the new player score
+    highscores.push(currentScore);
+
+    //combiming old and new players scores and adding them to storage
+    window.localStorage.setItem("high-score-list", JSON.stringify(highscores));
 }
 
 // Connect the savePlayerScoreInitials function to a click event
-submitScoreBtn.onclick=savePlayerScoreInitials;
+submitScoreBtn.onclick = savePlayerScoreInitials;
 
 
 
